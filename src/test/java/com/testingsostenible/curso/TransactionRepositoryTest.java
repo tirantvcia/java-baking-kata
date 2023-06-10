b23c5d805a0cd8fd3a961e7c8ea9351e6483ee5b
tirantvcia <tirantvcia@gmail.com>
2023-06-11 01:14:18 +0200

    refactor extract some actions to beforeeach

--- src/test/java/com/testingsostenible/curso/TransactionRepositoryTest.java
+++ src/test/java/com/testingsostenible/curso/TransactionRepositoryTest.java
@@ -5,22 +5,29 @@ import static org.mockito.Mockito.*;
 
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TransactionRepositoryTest {
 
+	String today;
+	Clock clock;
+	TransactionRepository repository;
+	
+	@BeforeEach
+	public void init() {
+		today = "25/03/2022";
+		clock = mock(Clock.class);
+		repository = new TransactionRepository(clock);
+	}
 	@Test
 	void storesDepositTransactionForGivenAmout() {
 		
-		String today = "25/03/2022";
 		int amount = 100;
-		Clock clock = mock(Clock.class);
-		
 		when(clock.todayAsString()).thenReturn(today);
-		TransactionRepository repository = new TransactionRepository(clock);
+		
 		repository.addDeposit(amount);
 		List<Transaction> transactions = repository.allTransactions();
-		
 		assertTrue(new Transaction(today, amount).equals(transactions.get(0)));
 	}
 
