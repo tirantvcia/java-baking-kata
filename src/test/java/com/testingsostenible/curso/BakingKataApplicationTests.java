9095316a998f4137250e7ed53e0b32728b45034e
tirantvcia <tirantvcia@gmail.com>
2023-06-12 11:26:58 +0200

    green, printsAccountStatementWithTransactionsThroughoutConsole

--- src/test/java/com/testingsostenible/curso/BakingKataApplicationTests.java
+++ src/test/java/com/testingsostenible/curso/BakingKataApplicationTests.java
@@ -1,9 +1,18 @@
 package com.testingsostenible.curso;
 
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -16,10 +25,12 @@ class BakingKataApplicationTests {
 	
 	private Account account = null;
 	private Clock clock;
+	@Captor
+	ArgumentCaptor<String> lineTransactionCaptor;
 
 	@BeforeEach
 	public void init () {
-		clock = new Clock();
+		clock = mock(Clock.class);
 		consoleSpy = mock(Console.class);
 		printer = new StatementPrinter(consoleSpy);
 		
@@ -31,16 +42,19 @@ class BakingKataApplicationTests {
 	@Test
 	void printsAccountStatementWithTransactionsThroughoutConsole() {
 
+		when(clock.todayAsString()).thenReturn("10/01/2022", "13/01/2022", "14/01/2022");
 		account.deposit(1000);
 		account.withdraw(500);
 		account.deposit(2000);
 
 		account.printStatement();
 
-		verify(consoleSpy).log("Date | Amount | Balance");
-		verify(consoleSpy).log("14/01/2022 | 2000 | 2500");
-		verify(consoleSpy).log("13/01/2022 | -500 | 500");
-		verify(consoleSpy).log("10/01/2022 | 1000 | 1000");
+		verify(consoleSpy, times(4)).log(lineTransactionCaptor.capture());
+		List<String> linesCaptured = lineTransactionCaptor.getAllValues();
+		assertTrue("Date | Amount | Balance".equals(linesCaptured.get(0)));
+		assertEquals("14/01/2022 | 2000.00 | 2500.00", linesCaptured.get(1));
+		assertTrue("13/01/2022 | -500.00 | 500.00".equals(linesCaptured.get(2)));
+		assertTrue("10/01/2022 | 1000.00 | 1000.00".equals(linesCaptured.get(3)));
 	}
 
 
